28b579faf9760e25b1878c79b3177bb829720efe
DontEatThemCookies <67893242+DontEatThemCookies@users.noreply.github.com>
2021-09-23 19:56:41 +0800

    Python

---
+++ Python/pyscreenshot/main.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# Above line is for Linux clients
+
+###############################################
+# MODIFY ONLY IF YOU KNOW WHAT YOU ARE DOING! #
+###############################################
+
+
+
+# Python program to take screenshots - v1 9-23-2021
+# Dependencies: numpy, opencv-python, pyautogui, pillow (not PIL)
+# python3 -m pip install [dependency]
+# Substitute "python3" to "py" or "python.exe" if it fails to execute.
+
+# Programmed in Python for Windows
+
+##################################################
+#               PYTHON DEPENDENCIES
+import numpy as np
+import cv2
+import pyautogui
+#
+##################################################
+#             BUILT-IN PYTHON MODULES
+import os
+import uuid 
+import ctypes
+import subprocess
+
+from tkinter import *
+from os import system, name
+from time import sleep
+#
+##################################################
+
+scriptname = os.path.basename(__file__)
+init = "Initialized! Script name: "
+alt = "Python Screenshot [MacOS/Linux/POSIX]"
+
+def title():
+
+    if name == 'nt':
+        _ = ctypes.windll.kernel32.SetConsoleTitleW("Python Screenshot [Windows]")
+    else:
+        _ = print(f'\33]0;{alt}\a', end='', flush=True)
+
+def quit(event):
+    exit() # Tkinter code, to be used in future versions
+
+title()
+
+# Initialization
+
+A = 1
+
+if A > 1:
+    print("Screenshot taken!")
+
+print(init+scriptname)
+print("")
+print("Python Screenshot - Fullscreen screenshots")
+print("")
+print("This script should be run directly and not from the command line.")
+print("This program will return to this screen automatically after each screenshot.")
+print("Image will be named randomly and saved to the folder containing this script.")
+print("")
+input("Press ENTER once ready to take the screenshot. Otherwise, CTRL + C to exit.")
+
+
+# This line closes window instances of this script prematurely in Windows.
+# It has no functional use for MacOS/Linux.
+#ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 0 )
+
+
+filename = str(uuid.uuid4()) # Define filename as a random UUID
+string2 = ".png" # Define the image format to be saved - changeable to JPG/JPEG
+name = filename+string2 # Concantenate the two strings to be used by CV2.
+
+# Take the screenshot using pyautogui
+image = pyautogui.screenshot()
+   
+# Convert to numpy array so it can be written to the disk.
+image = cv2.cvtColor(np.array(image),
+                     cv2.COLOR_RGB2BGR)
+   
+# Saves the image.
+cv2.imwrite(name, image)
+
+print("")
+with open('main.py') as infile:
+    exec(infile.read())
+# exec(open('main.py').read())
+# old method of opening file
+
+# EOF
---
+++ readme.txt
@@ -0,0 +1,37 @@
+The installers for Python are up-to-date as of September 23, 2021
+It is recommended to download from python.org if newer version are available.
+
+
+
+INSTRUCTIONS FOR WINDOWS 8/WINDOWS 8.1/WINDOWS 10/WINDOWS 11
+1. Go to "python-installers" folder and run the Python installer (python-3.9.7-amd64.exe)
+   Check the option that says "Add Python 3.9 to PATH" or the script won't work properly.
+2. Restart your PC
+3. Open Command Prompt (Press Windows Key, type "cmd" and open it)
+4. Type in the following commands. Wait for one command to finish before entering another one:
+   
+   py -m pip install numpy
+   py -m pip install opencv-python
+   py -m pip install pyautogui
+   py -m pip install pillow
+
+5. Run main.py
+
+
+
+
+
+INSTRUCTIONS FOR WINDOWS 7 OR OLDER (DO NOT USE IF YOU HAVE WINDOWS 8/8.1/10)
+1. Go to "python-installers" folder and run the Python installer (python-3.8.10-amd64.exe)
+   If you have a x86/32 bit computer, use (python-3.8.10.exe)
+   Check the option that says "Add Python 3.8 to PATH" or the script won't work properly.
+2. Restart your PC
+3. Open Command Prompt (Press Windows Key, type "cmd" and open it)
+4. Type in the following commands. Wait for one command to finish before entering another one:
+   
+   py -m pip install numpy
+   py -m pip install opencv-python
+   py -m pip install pyautogui
+   py -m pip install pillow
+
+5. Run main.py

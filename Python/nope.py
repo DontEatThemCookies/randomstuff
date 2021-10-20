19897e6ca3f3ce2f68befbe4fb304fdcb2f0c187
David Costell <davidcostell44@gmail.com>
2021-10-21 02:37:04 +0800

    Nope implementation

---
+++ Python/nope.py
@@ -0,0 +1,9 @@
+# Dual-line implementation of the Nope esolang - 10/20/21
+
+# Python 3.x implementation
+a = input()
+if a == a: print('Nope.')
+
+# Python 2.x implementation
+# a = raw_input()
+# if a == a: print 'Nope.'
